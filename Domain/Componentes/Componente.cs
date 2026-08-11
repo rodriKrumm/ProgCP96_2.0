70a99a97cb044614fe47979574eb3a0cae029236
Rodrigo Krummenauer <rodrigovisus@gmail.com>
2026-08-11 11:58:26 -0300

    Criada classe Componente

---
+++ Domain/Componentes/Componente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROGCP96_V1._1_.Domain.Componentes
+{
+    internal class Componente
+    {
+        public int Codigo { get; set; }
+        public Image Imagem { get; set; }
+        public Posicao posicao { get; set; }
+    }
+}
+        
