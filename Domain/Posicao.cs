89ad5f4e73f0612a9ef562d54765c311c64a9786
Rodrigo Krummenauer <rodrigovisus@gmail.com>
2026-08-11 11:54:37 -0300

    Criada classe Posicao

---
+++ Domain/Posicao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROGCP96_V1._1_.Domain
+{
+    public class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao()
+        {
+            
+        }
+
+        public Posicao(int linha, int coluna)
+        {
+            this.Linha = linha;
+            this.Coluna = coluna;
+        }
+    }
+}
